9d3b9352c1c3b21227a1ee12cbb49fc64207eadb
Ahmed Sherif <ahmsherif23@gmail.com>
2024-08-10 02:49:51 +0300

    .

--- AhmedSherif C43 React Assignment 1 [Start FrameWork]/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Route-FullStack-Diploma/tree/main/AhmedSherif%20C43%20React%20Assignment%201%20%5BStart%20FrameWork%5D/",
-})
